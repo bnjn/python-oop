14485ccd5010098842215c7b38b1efc7e63305c6
Benjamin Johnson <50123967+bnjn@users.noreply.github.com>
2023-01-18 12:43:15 +0000

    updated total price method

--- main.py
+++ main.py
@@ -3,17 +3,13 @@ class Item:
     self.name = name
     self.price = price
     self.quantity = quantity
-  def calculate_total_price(self, x, y):
-    return x * y
+  def calculate_total_price(self):
+    return self.price * self.quantity
 
 
-item1 = Item('Phone', 100)
+item1 = Item('Phone', 100, 1)
 
-item2 = Item('Laptop', 1000)
+item2 = Item('Laptop', 1000, 3)
 
-print(item1.name)
-print(item2.name)
-print(item1.price)
-print(item2.price)
-print(item1.quantity)
-print(item2.quantity)
+print(item1.calculate_total_price())
+print(item2.calculate_total_price())
