abf7d366d10ea4625436b7a30c9b2f3ef6494a48
Benjamin Johnson <50123967+bnjn@users.noreply.github.com>
2023-01-30 16:37:33 +0000

    added Phone class

--- main.py
+++ main.py
@@ -46,8 +46,24 @@ class Item:
       return False
 
   def __repr__(self):
-    return f"Item('{self.name}', {self.price}, {self.quantity})"
+    return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
 
-Item.instantiate_from_csv()
+class Phone(Item):
+  def __init__(self, name: str, price: float, quantity: int=0, broken_phones: int=0):
+    # Call to super function to access all attb/methods
+    super().__init__(
+      name, price, quantity
+    )
+
+    # Run validations on the received arguments
+    assert broken_phones >= 0, f'Broken phones {broken_phones} is not greater than or equal to zero!'
+
+    # Assign to self object
+    self.broken_phones = broken_phones
+
+phone1 = Phone('phonev10', 500, 5, 1)
+print(phone1.calculate_total_price())
+phone2 = Phone('phonev30', 700, 5, 1)
+
 print(Item.all)
-print(Item.check_integer(7.0))
+print(Phone.all)
