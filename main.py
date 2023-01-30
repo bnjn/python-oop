3646b8270cc650ef1416e7e6cc712de6444d64e5
Benjamin Johnson <50123967+bnjn@users.noreply.github.com>
2023-01-30 11:40:46 +0000

    class and instance attributes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 class Item:
+  pay_rate = 0.8 # The pay rate after 20 percent discount
+
   def __init__(self, name: str, price: float, quantity: int=0):
     # Run validations on the received arguments
     assert price >= 0, f'Price {price} is not greater than or equal to zero!'
@@ -12,9 +14,14 @@ class Item:
   def calculate_total_price(self):
     return self.price * self.quantity
 
+  def apply_discount(self):
+    self.price = self.price * self.pay_rate
 
 item1 = Item('Phone', 100, 1)
-item2 = Item('Laptop', 1000, 3)
+item1.apply_discount()
+print(item1.price)
 
-print(item1.calculate_total_price())
-print(item2.calculate_total_price())
+item2 = Item('Laptop', 1000, 3)
+item2.pay_rate = 0.7
+item2.apply_discount()
+print(item2.price)
