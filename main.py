c33170a0edd76f58538f3412c2a24708efa593da
Benjamin Johnson <50123967+bnjn@users.noreply.github.com>
2023-01-30 11:56:28 +0000

    list of Item objects. __repr__

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 class Item:
   pay_rate = 0.8 # The pay rate after 20 percent discount
-
+  all:list = []
   def __init__(self, name: str, price: float, quantity: int=0):
     # Run validations on the received arguments
     assert price >= 0, f'Price {price} is not greater than or equal to zero!'
@@ -11,17 +11,22 @@ class Item:
     self.price = price
     self.quantity = quantity
 
+    # Actions to execute
+    Item.all.append(self)
+
   def calculate_total_price(self):
     return self.price * self.quantity
 
   def apply_discount(self):
     self.price = self.price * self.pay_rate
 
-item1 = Item('Phone', 100, 1)
-item1.apply_discount()
-print(item1.price)
+  def __repr__(self):
+    return f"Item('{self.name}', {self.price}, {self.quantity})"
 
-item2 = Item('Laptop', 1000, 3)
-item2.pay_rate = 0.7
-item2.apply_discount()
-print(item2.price)
+item1 = Item("Phone", 100, 1)
+item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
+
+print(Item.all)
