73860c8d9df2f6f58c5d985c8b9bc632fca3a966
Benjamin Johnson <50123967+bnjn@users.noreply.github.com>
2023-01-30 17:33:25 +0000

    example of abstraction with send_email and __private_function

--- item.py
+++ item.py
@@ -34,7 +34,7 @@ class Item:
     return self.__price
 
   def apply_discount(self):
-    self.__price = self.__price * self.pay_rate
+    self.__price *= self.pay_rate
 
   def apply_increment(self, increment_value):
     self.__price += self.__price * increment_value
@@ -67,3 +67,21 @@ class Item:
 
   def __repr__(self):
     return f"{self.__class__.__name__}('{self.name}', {self.__price}, {self.quantity})"
+
+  def __connect(self, smpt_server):
+    pass
+
+  def __prepare_body(self):
+    return f"""
+    Hello
+    We have {self.quantity} of {self.name}.
+    Thanks
+    """
+
+  def __send(self):
+    pass
+
+  def send_email(self):
+    self.__connect('somesmtpserver')
+    self.__prepare_body()
+    self.__send()
